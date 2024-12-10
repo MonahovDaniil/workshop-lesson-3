e4697ee192bb238d2193f45fdcb25fe45bfc030e
Daniil <7102771@gmail.com>
2024-12-10 18:59:53 +0300

    init commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        byte a = 1;
+        short b = 2;
+//        int c = 4;
+//        long d = 8L;
+//        float e = 1.0f;
+//        double f = 1.0d;
+//        char g = 'a';
+//        boolean bool = true;
+//
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+//        System.out.println(d);
+//        System.out.println(e);
+//        System.out.println(f);
+//        System.out.println(g);
+//        System.out.println(bool);
+//        if(a>10 & b>5) {
+//
+//        }else{
+//
+//        }
+
+    }
+}
